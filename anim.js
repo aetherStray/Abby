05d21b7224ba0e85cd952abcd9fb2dd2e9e7b486
AetherStray <AetherStray2023@gmail.com>
2024-09-21 21:12:02 -0600

    terminado3

--- anim.js
+++ anim.js
@@ -9,42 +9,42 @@ var lyricsData = [
   { text: "un amante en abandono quien va a regresar", time: 22 },
   { text: "en su frente el colmo de la triste humanidad", time: 27 },
   { text: "Con un ser converso", time: 33 },
-  { text: "de otro universo", time: 37 },
+  { text: "de otro universo", time: 35 },
 
-  { text: "Algún dia te dejaré", time: 45 },
-  { text: "por siempre de buscar", time: 47 },
-  { text: "lo digo porque sé ", time: 50 },
-  { text: "nos vamos a encontrar", time: 53 },
-  { text: "en un púrpura atardecer ", time: 56 },
-  { text: "que no se apagará", time: 58 },
-  { text: "todos van a ver", time: 62 },
-  { text: "un nuevo principío ", time: 64 },
-  { text: "un encuentro del...", time: 67 },
-  { text: "tercer tipo", time: 69 },
+  { text: "Algún dia te dejaré", time: 43 },
+  { text: "por siempre de buscar", time: 45 },
+  { text: "lo digo porque sé ", time: 48 },
+  { text: "nos vamos a encontrar", time: 51 },
+  { text: "en un púrpura atardecer ", time: 54 },
+  { text: "que no se apagará", time: 56 },
+  { text: "todos van a ver", time: 60 },
+  { text: "un nuevo principío ", time: 62 },
+  { text: "un encuentro del...", time: 65 },
+  { text: "tercer tipo", time: 67 },
 
-  { text: "¿Dónde se origina el viento que se oye pasar?", time: 82},
-  { text: "¿Quién sabe de donde proviene y donde irá a parar?", time: 87 },
-  { text: "así es andar", time: 93 },
-  { text: "así es la libertad", time: 97 },
+  { text: "¿Dónde se origina el viento que se oye pasar?", time: 80},
+  { text: "¿Quién sabe de donde proviene y donde irá a parar?", time: 85 },
+  { text: "así es andar", time: 91 },
+  { text: "así es la libertad", time: 95 },
 
-  { text: "Algún dia te dejaré", time: 105 },
-  { text: "por siempre de buscar", time: 107 },
-  { text: "lo digo porque sé ", time: 110 },
-  { text: "nos vamos a encontrar", time: 113 },
-  { text: "en un púrpura atardecer ", time: 116 },
-  { text: "que no se apagará", time: 118 },
-  { text: "todos van a ver", time: 121 },
-  { text: "un nuevo principío ", time: 124 },
-  { text: "un encuentro del...", time: 126 },
-  { text: "tercer tipo", time: 129 },
+  { text: "Algún dia te dejaré", time: 103 },
+  { text: "por siempre de buscar", time: 105 },
+  { text: "lo digo porque sé ", time: 108 },
+  { text: "nos vamos a encontrar", time: 111 },
+  { text: "en un púrpura atardecer ", time: 114 },
+  { text: "que no se apagará", time: 116 },
+  { text: "todos van a ver", time: 119 },
+  { text: "un nuevo principío ", time: 122 },
+  { text: "un encuentro del...", time: 124 },
+  { text: "tercer tipo", time: 127 },
 
 
   { text: "Todos te van a ver ", time: 168 },
   { text: "tambien los que no me creen", time: 170 },
-  { text: "un nuevo principio ", time: 173 },
-  { text: "sin estereotipos", time: 176 },
-  { text: "un encuentro del...", time: 179 },
-  { text: "tercer tipo ", time: 181},
+  { text: "un nuevo principio ", time: 171 },
+  { text: "sin estereotipos", time: 174 },
+  { text: "un encuentro del...", time: 177 },
+  { text: "tercer tipo ", time: 179},
 ];
 
 // Animar las letras
@@ -56,7 +56,7 @@ function updateLyrics() {
 
   if (currentLine) {
     // Calcula la opacidad basada en el tiempo en la línea actual
-    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var fadeInDuration = 0; // Duración del efecto de aparición en segundos
     var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
 
     // Aplica el efecto de aparición
@@ -71,16 +71,3 @@ function updateLyrics() {
 
 setInterval(updateLyrics, 100);
 
-//funcion titulo
-// Función para ocultar el título después de 216 segundos
-function ocultarTitulo() {
-  var titulo = document.querySelector(".titulo");
-  titulo.style.animation =
-    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
-  setTimeout(function () {
-    titulo.style.display = "none";
-  }, 3000); // Espera 3 segundos antes de ocultar completamente
-}
-
-// Llama a la función después de 216 segundos (216,000 milisegundos)
-setTimeout(ocultarTitulo, 216000);
